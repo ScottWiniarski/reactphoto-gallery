3ff4f2e7ad52ce72cf0886854453a0ccf1964652
Scott Winiarski <Scott1w1@hotmail.com>
2023-12-12 20:25:40 -0600

    Implemented Camera Functionality Completed Basic Photo Gallery tutorial

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,7 @@
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import React, { useState } from 'react';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
 import PageHeader from '../components/PageHeader';
 import { camera, trash, close } from 'ionicons/icons';
 import {
@@ -19,6 +21,9 @@ import {
 } from '@ionic/react';
 
 const Tab2: React.FC = () => {
+  const {photos, takePhoto, deletePhoto} = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
+
   return (
     <IonPage>
       <PageHeader name='Photos'></PageHeader>
@@ -32,11 +37,42 @@ const Tab2: React.FC = () => {
       </IonContent>
 
       <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size='6' key={photo.filepath}>
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath}/>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
         </IonFab>
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[
+            {
+              text: 'Delete',
+              role: 'destructive',
+              icon: trash,
+              handler: () => {
+                if (photoToDelete) {
+                  deletePhoto(photoToDelete);
+                  setPhotoToDelete(undefined);
+                }
+              },
+            },
+            {
+              text: 'Cancel',
+              icon: close,
+              role: 'cancel',
+            },
+          ]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
       </IonContent>
     </IonPage>
   );
